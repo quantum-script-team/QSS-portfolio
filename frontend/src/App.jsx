53c5d7ed8008e77105992b4331b3664fb53592d7
Nicolas Stucky <nicolastucky2005@gmail.com>
2024-02-16 13:38:35 -0300

    carrusel tech

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CarrUsel from "../src/carrusel/carr"
+function App() {
+
+  return (
+    <>
+      <div>
+        < CarrUsel />
+      </div>
+    </>
+  )
+}
+
+export default App
