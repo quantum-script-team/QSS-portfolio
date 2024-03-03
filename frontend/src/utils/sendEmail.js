3df7cd65283835141a13ead964dee0bab34c8043
MayDenhoff <mayradenhoff@gmail.com>
2024-03-03 19:50:36 -0300

    cambios en sendEmail

--- frontend/src/utils/sendEmail.js
+++ frontend/src/utils/sendEmail.js
@@ -16,6 +16,7 @@ export const sendEmail = (error,form, setFormData, navigate) => {
     if (checkMail(error)) {
 
       emailjs
+        // .sendForm(service_id, "fsag", form.current, {
         .sendForm(service_id, template, form.current, {
           publicKey: public_key,
         })
@@ -26,38 +27,41 @@ export const sendEmail = (error,form, setFormData, navigate) => {
                   icon: "success",
                   text: "Su consulta fue enviada con exito, pronto nos pondremos en contacto.",
                   confirmButtonText: 'Aceptar',
+                  confirmButtonColor: "#2cb67d",
+                  
                   didOpen: () => {
-                      const acceptButton = Swal.getConfirmButton();
+                    const acceptButton = Swal.getConfirmButton();
                       acceptButton.addEventListener('click', () => {
                         
-   
-                          setFormData({
-                            name: "",
-                            lastName: "",
-                            email: "",
-                            phone: "",
-                            bussines: "",
-                            message: "",
-                            country: ""
-                          })
-                          setTimeout(() => {
-                              window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll hacia arriba después de 2 segundos
-                            navigate("/");
+                        
+                        setFormData({
+                          name: "",
+                          lastName: "",
+                          email: "",
+                          phone: "",
+                          bussines: "",
+                          message: "",
+                          country: ""
+                        })
+                        setTimeout(() => {
+                          window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll hacia arriba después de 2 segundos
+                          navigate("/");
                         }, 2000);
-                    });
+                      });
+                    }
+                  },
+                  
+                  )
+                  
                 }
-            },
-              
-              )
-
-          }
           ,
           
           (error) => {
             Swal.fire({
-                icon: "warning",
-                html: `<p>Su consulta no pudo ser enviada. Puede mandarnos su consulta por este e-mail: <a href=mailto:${"contact.quantumscript@gmail.com"}>contact.quantumscript@gmail.com</a></p>`,
-              })          },
-        );
-    }
-  };
+              icon: "warning",
+              html: `<p>Su consulta no pudo ser enviada. Puede mandarnos su consulta por este e-mail: <a href=mailto:${"contact.quantumscript@gmail.com"}>contact.quantumscript@gmail.com</a></p>`,
+              confirmButtonColor: "#2cb67d",
+            })          },
+            );
+          }
+        };
